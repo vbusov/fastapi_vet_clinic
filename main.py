da536cd98b6cb0880f2047a4255ca369451cb9a1
Vlad <30292919+vbusov@users.noreply.github.com>
2023-11-08 00:36:43 +0300

    Add files

---
+++ main.py
@@ -0,0 +1,86 @@
+import time
+from enum import Enum
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import PlainTextResponse
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+
+class DogType(str, Enum):
+    terrier = "terrier"
+    bulldog = "bulldog"
+    dalmatian = "dalmatian"
+
+
+class Dog(BaseModel):
+    name: str
+    pk: int
+    kind: DogType
+
+
+class Timestamp(BaseModel):
+    id: int
+    timestamp: int
+
+
+dogs_db = {
+    0: Dog(name='Bob', pk=0, kind='terrier'),
+    1: Dog(name='Marli', pk=1, kind="bulldog"),
+    2: Dog(name='Snoopy', pk=2, kind='dalmatian'),
+    3: Dog(name='Rex', pk=3, kind='dalmatian'),
+    4: Dog(name='Pongo', pk=4, kind='dalmatian'),
+    5: Dog(name='Tillman', pk=5, kind='bulldog'),
+    6: Dog(name='Uga', pk=6, kind='bulldog')
+}
+
+post_db = [
+    Timestamp(id=0, timestamp=12),
+    Timestamp(id=1, timestamp=10)
+]
+
+
+@app.get('/')
+def root() -> str:
+    return PlainTextResponse("Welcome to the clinic!")
+
+
+@app.post('/')
+def get_post() -> Timestamp:
+    last_id = max(timestamp.id for timestamp in post_db)
+    post_timestamp = Timestamp(id=last_id + 1, timestamp=round(time.time()))
+    post_db.append(post_timestamp)
+    return post_timestamp
+
+
+@app.get('/dog')
+def get_dogs(kind: DogType = None) -> list[Dog]:
+    dogs = [dogs_db[pk] for pk in dogs_db]
+    if kind:
+        dogs = [dog for dog in dogs if dog.kind == kind]
+    return [dog for dog in dogs]
+
+
+@app.post('/dog')
+def create_dog(dog: Dog) -> Dog:
+    if dog.pk in dogs_db:
+        raise HTTPException(status_code=422, detail="Key already exists")
+    dogs_db[dog.pk] = dog
+    return dog
+
+
+@app.get('/dog/{pk}')
+def get_dog_by_pk(pk: int) -> Dog:
+    return dogs_db[pk]
+
+
+@app.patch('/dog/{pk}')
+def update_dog(pk: int, dog: Dog) -> Dog:
+    if dog.pk not in dogs_db:
+        raise HTTPException(status_code=422, detail="Key doesn't exist")
+    if dog.pk != pk:
+        raise HTTPException(status_code=422, detail="Key mismatch between path and body")
+    dogs_db[pk] = dog
+    return dog
